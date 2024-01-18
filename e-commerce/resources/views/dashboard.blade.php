338880b90b6a04d166110f9bfb23d9bb1b8c6152
enoyat <50427196+enoyat@users.noreply.github.com>
2024-01-18 20:51:58 +0700

    1

--- e-commerce/resources/views/dashboard.blade.php
+++ e-commerce/resources/views/dashboard.blade.php
@@ -206,61 +206,6 @@
     </header>
     <!-- Header Section End -->
 
-    <!-- Hero Section Begin -->
-    <section class="hero">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="hero__categories">
-                        <div class="hero__categories__all">
-                            <i class="fa fa-bars"></i>
-                            <span>All Category</span>
-                        </div>
-                        <ul>
-                            <?php foreach ($kategori as $key) { ?>  
-                                <li class="nav-item">
-                                  <a class="nav-link" href="{{ URL::to('home/kategori/'.$key->kdkategori) }}">{{ $key->namakategori }}</a>
-                                </li>
-                             
-                          <?php } ?>                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-9">
-                    <div class="hero__search">
-                        <div class="hero__search__form">
-                    
-                            <form action="/home/search">
-                                @csrf
-                                
-                                
-                                <input type="text" class="form-control" placeholder="Search" name="katakunci" id="katakunci" 
-                                    value="{{ Session::get('katakunci') }}" size="50">
-                                <button type="submit" class="site-btn">SEARCH</button>
-                            </form>
-                        </div>
-                        <div class="hero__search__phone">
-                            <div class="hero__search__phone__icon">
-                                <i class="fa fa-phone"></i>
-                            </div>
-                            <div class="hero__search__phone__text">
-                                <h5>+65 11.188.888</h5>
-                                <span>support 24/7 time</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="hero__item set-bg" data-setbg="img/hero/banner.jpg">
-                        <div class="hero__text">
-                            <span>FRUIT FRESH</span>
-                            <h2>Vegetable <br />100% Organic</h2>
-                            <p>Free Pickup and Delivery Available</p>
-                            <a href="#" class="primary-btn">SHOP NOW</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Hero Section End -->
     <div class="container">
     @yield('content')
     </div>
